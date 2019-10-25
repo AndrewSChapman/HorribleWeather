686161916fe904ee517c0e031bf46b192b4601cf
Andrew Chpaman <andy@andychapman.net>
2019-10-25 17:34:44 +0100

    https

---
+++ frontend/horribleweather/dist/precache-manifest.27c0025c59f7780ed08c523221b8da0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d9640c287da7858375",
+    "url": "/css/app.d8c71d64.css"
+  },
+  {
+    "revision": "51572f9a07ada53998d8a999c6d9b231",
+    "url": "/img/storm.51572f9a.svg"
+  },
+  {
+    "revision": "4a3fc07b9e4e7d6ac11208cba76b300f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4b210adb7b55396f99c",
+    "url": "/js/about.60c82222.js"
+  },
+  {
+    "revision": "b4d9640c287da7858375",
+    "url": "/js/app.7e5ccf54.js"
+  },
+  {
+    "revision": "7dfb7a8e157de2f6cdb6",
+    "url": "/js/chunk-vendors.7722ac69.js"
+  },
+  {
+    "revision": "fe5fd2d633b0a93d7f67bed77bf6545c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
